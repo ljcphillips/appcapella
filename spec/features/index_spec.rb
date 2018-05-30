1717592f5c632c9f6e85569c75f7e07ab396a188
Lorraine Philllips <ljcphillips@gmail.com>
2018-05-30 15:56:18 +0100

    multiplay first test

--- spec/features/index_spec.rb
+++ spec/features/index_spec.rb
@@ -1,12 +1,17 @@
 feature 'page should multiple videos' do
   scenario 'first video is displayed' do
-    visit ('/')
+    visit '/'
     visit page.find('#video1')[:src]
     expect(page.status_code).to be(200)
   end
   scenario 'second video is displayed' do
-    visit ('/')
+    visit '/'
     visit page.find('#video2')[:src]
     expect(page.status_code).to be(200)
+  end
+  scenario 'both videos should start playing on one click' do
+    visit '/'
+    find("#playButton").click
+    video = find("#videoElement")
   end
 end
