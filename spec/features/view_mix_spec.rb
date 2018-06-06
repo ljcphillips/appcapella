f8e9350979e123a2373f297304bb555850949f30
Daniel Tooke <daniel.tooke@gmail.com>
2018-06-06 11:03:36 +0100

    Rewrote feature test for recording video

--- spec/features/view_mix_spec.rb
+++ spec/features/view_mix_spec.rb
@@ -1,9 +1,18 @@
-feature 'page should show the mixed video' do
-
-  scenario 'Mix video player is displayed' do
-    visit '/view_mix'
-    visit page.find('#mix')[:src]
-    expect(page.status_code).to be(200)
+feature 'It should be possible to record four videos and mix them together' do
+  scenario 'Uploading four videos and pressing mix produces a new video', :js => true do
+    Capybara.current_driver = :chrome
+    1.upto(4) do |num|
+      visit ('/')
+      find("#record").click
+      sleep 1
+      find("#stop").click
+      page.accept_prompt(with: "#{num}")
+    end
+    find("#mix").click
+    page.evaluate_script("$('#mix_video').get(0).play()")
+    get_paused = "$('#mix_video')[0].paused;"
+    is_paused = page.evaluate_script(get_paused)
+    expect(is_paused).to eq(false)
+    Capybara.use_default_driver
   end
-
 end
