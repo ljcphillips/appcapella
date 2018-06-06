f581ed7d22a69a44b114c40192abbff6172f5e3c
Marwa Elali <marwaelali@gmail.com>
2018-06-06 13:08:40 +0100

    test of test

--- spec/features/facebook_share_spec.rb
+++ spec/features/facebook_share_spec.rb
@@ -1,25 +1,33 @@
+require_relative '../../fbdetails'
+
 feature 'User can share video on facebook' do
   scenario 'facebook share button posts mixed video on user profile' do
     visit '/view_mix'
+    # find('#fb-share').click
 
-    login_window = window_opened_by do
-      find('#fb-share').click
-    end
-    within_window(login_window) do
-      fill_in('email', with: 'fbemail')
-      fill_in('pass', with: 'fbpassword')
+    within('#fb-share') do
+      fill_in('email', with: FBEMAIL)
+      fill_in('pass', with: FBPASSWORD)
       find('#u_0_0').click
       find('#u_0_1x').click
-      expect('www.facebook.com/profile').to have_content('appcapella.herokuapp.com')
     end
 
-    # within_window(page.driver.browser.window_handles.last) do
-    #   fill_in('email', with: 'marwaelali@gmail.com')
-    #   fill_in('pass', with: '47XJ99qi.')
+    # main = page.switch_to_window page.windows.first
+    # popup = page.switch_to_window page.windows.last
+
+    # main = page.driver.current_window_handle.first
+    # popup = page.driver.current_window_handle.last
+
+
+    # login_window = window_opened_by do
+    #   click_button('fb-share')
+    # end
+    # within_window(popup) do
+    #   fill_in('email', with: FBEMAIL)
+    #   fill_in('pass', with: FBPASSWORD)
     #   find('#u_0_0').click
     #   find('#u_0_1x').click
-    #   visit 'facebook.com/marwa.ea.7'
-    #   expect(page).to have_content('appcapella.herokuapp.com')
     # end
+    # expect('www.facebook.com/' + FBPROFILE).to have_content('appcapella.herokuapp.com')
   end
 end
