19c956c3802885366c5b2498671938d98d47aeea
Marwa Elali <marwaelali@gmail.com>
2018-06-07 11:31:15 +0100

    option of two tests for fb button - both tests passing

--- spec/features/facebook_share_spec.rb
+++ spec/features/facebook_share_spec.rb
@@ -14,8 +14,17 @@ feature 'User can share video on facebook' do
     find('#u_0_0').click
     # Post to Facebook button
     find('#u_0_1x').click
-
-
-    # expect('www.facebook.com/' + FBPROFILE).to have_content('appcapella.herokuapp.com')
+    visit 'http://facebook.com/' + FBPROFILE
+    expect(page).to have_content('appcapella.herokuapp.com')
   end
+
+  # A more simple test, perhaps more secure so we don't have to store our details?
+  # scenario 'facebook share button posts mixed video on user profile', js: true do
+  #   visit '/view_mix'
+  #   find('#fb-share').click
+  #
+  #   # Switch focus to popup window
+  #   page.driver.switch_to_window(page.driver.window_handles.last)
+  #   expect(page).to have_content('Facebook')
+  # end
 end
