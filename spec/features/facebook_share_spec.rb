63c27de9c799a4cfab3e1e4af971ccfdacfae647
Marwa Elali <marwaelali@gmail.com>
2018-06-07 11:15:12 +0100

    share button test forgot to add to previous commit

--- spec/features/facebook_share_spec.rb
+++ spec/features/facebook_share_spec.rb
@@ -1,33 +1,21 @@
 require_relative '../../fbdetails'
 
 feature 'User can share video on facebook' do
-  scenario 'facebook share button posts mixed video on user profile' do
+  scenario 'facebook share button posts mixed video on user profile', js: true do
     visit '/view_mix'
-    # find('#fb-share').click
+    find('#fb-share').click
 
-    within('#fb-share') do
-      fill_in('email', with: FBEMAIL)
-      fill_in('pass', with: FBPASSWORD)
-      find('#u_0_0').click
-      find('#u_0_1x').click
-    end
+    # Switch focus to popup window
+    page.driver.switch_to_window(page.driver.window_handles.last)
 
-    # main = page.switch_to_window page.windows.first
-    # popup = page.switch_to_window page.windows.last
-
-    # main = page.driver.current_window_handle.first
-    # popup = page.driver.current_window_handle.last
+    fill_in('email', with: FBEMAIL)
+    fill_in('pass', with: FBPASSWORD)
+    # Log In button
+    find('#u_0_0').click
+    # Post to Facebook button
+    find('#u_0_1x').click
 
 
-    # login_window = window_opened_by do
-    #   click_button('fb-share')
-    # end
-    # within_window(popup) do
-    #   fill_in('email', with: FBEMAIL)
-    #   fill_in('pass', with: FBPASSWORD)
-    #   find('#u_0_0').click
-    #   find('#u_0_1x').click
-    # end
     # expect('www.facebook.com/' + FBPROFILE).to have_content('appcapella.herokuapp.com')
   end
 end
