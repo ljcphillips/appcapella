2bd67af74cb6e3a835798fd65e5e7906accf6583
Marwa Elali <marwaelali@gmail.com>
2018-06-07 11:58:49 +0100

    removed test that requires details

--- spec/features/facebook_share_spec.rb
+++ spec/features/facebook_share_spec.rb
@@ -7,24 +7,6 @@ feature 'User can share video on facebook' do
 
     # Switch focus to popup window
     page.driver.switch_to_window(page.driver.window_handles.last)
-
-    fill_in('email', with: FBEMAIL)
-    fill_in('pass', with: FBPASSWORD)
-    # Log In button
-    find('#u_0_0').click
-    # Post to Facebook button
-    find('#u_0_1x').click
-    visit 'http://facebook.com/' + FBPROFILE
-    expect(page).to have_content('appcapella.herokuapp.com')
+    expect(page).to have_content('Facebook')
   end
-
-  # A more simple test, perhaps more secure so we don't have to store our details?
-  # scenario 'facebook share button posts mixed video on user profile', js: true do
-  #   visit '/view_mix'
-  #   find('#fb-share').click
-  #
-  #   # Switch focus to popup window
-  #   page.driver.switch_to_window(page.driver.window_handles.last)
-  #   expect(page).to have_content('Facebook')
-  # end
 end
